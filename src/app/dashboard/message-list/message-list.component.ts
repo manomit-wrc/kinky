884f0b09461361ac06c3beeeba6a3bae13fa01b5
Apple <apple@Apples-Mac-mini.local>
2018-11-09 17:57:05 +0530

    amir

--- src/app/dashboard/message-list/message-list.component.ts
+++ src/app/dashboard/message-list/message-list.component.ts
@@ -14,7 +14,9 @@ import * as jwt_decode from 'jwt-decode';
 export class MessageListComponent implements OnInit {
 
   messageList: any;
-
+  limit:any = 10;
+  order:any = 'requset_add';
+  reverse:any = false;
   constructor(
     private router: ActivatedRoute,
     private auth: AuthenticationService,
@@ -25,9 +27,34 @@ export class MessageListComponent implements OnInit {
     .pipe(
       tap(data => {
         this.messageList = data.messageList;
-        
       })
     ).subscribe(noop);
+  }
+
+
+  removeAll(id){
+    if(confirm("Are you sure to delete")) {
+      this.auth.removeAllMesasage(id)
+      .pipe(
+        tap(data => {
+
+          if(data.code ===200){
+            this.auth.message_list()
+    .pipe(
+      tap(datas => {
+        this.messageList = datas.messageList;
+      })
+    ).subscribe(noop);
+          }
+
+        })
+      ).subscribe(noop);
+    }
+
+  }
+
+  loadMore(){
+this.limit +=10;
   }
 
 }
